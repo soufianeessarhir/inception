01696dead1713453469e5df58834d1d39d567f59
Soufiane Essarhir <soufianeessarhir@gmail.com>
2025-09-30 20:21:49 +0100

    nginx in progress

---
+++ srcs/requirements/nginx/tools/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if [ ! -f "${CERTS_PATH}/nginx.cert"  || ! -f "${CERTS_PATH}/nginx.key" ] ;then #the check is of restart cases
+    openssl req -nodes -x509 -days 365  -newkey rsa:2048\
+    -out ${CERTS_PATH}/nginx.cert \
+    -keyout ${CERTS_PATH}/nginx.key 
+
+fi
