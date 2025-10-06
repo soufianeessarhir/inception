d60f84da148293567de8361fafba63eea99e43fa
soufiane essarhir <soufianessarhir@gmail.com>
2025-10-06 15:31:22 +0100

    push all

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -2,6 +2,5 @@
 if [ ! -f "${CERTS_PATH}/nginx.cert"  || ! -f "${CERTS_PATH}/nginx.key" ] ;then #the check is of restart cases
     openssl req -nodes -x509 -days 365  -newkey rsa:2048\
     -out ${CERTS_PATH}/nginx.cert \
-    -keyout ${CERTS_PATH}/nginx.key 
-
+    -keyout ${CERTS_PATH}/nginx.key  #paths should be defined explecitly
 fi
