1bc365d724636230261ee52a608fcc76eb58cc7f
soufianeessarhir <soufianeessarhir@gmail.com>
2025-10-14 10:35:47 +0100

    add wordpress

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -3,23 +3,21 @@ DB_PASSWORD=$(cat /run/secrets/db_password.txt)
 DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password.txt)
 
 if [ ! -d /var/lib/mysql/mysql ]; then
-
 mariadb-install-db --user=mysql --datadir=/var/lib/mysql >> /dev/null
-
 cat << EOF > /tmp/init_secure.sql
 USE mysql;
 FLUSH PRIVILEGES;
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost','127.0.0.1','::1');
-ALTER USER 'root'@'localhost' IDENTIFIED BY ${DB_ROOT_PASSWORD};
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 CREATE DATABASE IF NOT EXISTS ${DB_NAME} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY ${DB_PASSWORD};
-GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO ${DB_USER};
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
-mariadb --user=mysql  < /tmp/init_secure.sql
+mariadbd --user=mysql --bootstrap --verbose=0 < /tmp/init_secure.sql
 rm -rf /tmp/init_secure.sql
 fi
 
