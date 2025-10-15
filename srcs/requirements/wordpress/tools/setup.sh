bb45f5d9289ea943750ffce810cfeb711ba4e97b
soufianeessarhir <soufianeessarhir@gmail.com>
2025-10-15 21:35:04 +0100

    create wordpress container

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,9 +1,46 @@
+#!/bin/sh
+
 DB_PASSWORD=$(cat /run/secrets/db_password)
 WP_ADMIN_PASSWORD=$(cat /run/secrets/credentials)
-if [! -f "/var/www/html/index.php"]; then 
+
+if [ ! -f "/var/www/html/index.php" ]; then 
     cd /var/www/html
-     wget https://wordpress.org/wordpress-6.4.3.tar.gz
-    tar -xzf wordpress-6.4.3.tar.gz --strip-components=1
-    rm wordpress-6.4.3.tar.gz
+    wget -q https://wordpress.org/latest.tar.gz
+    tar -xzf latest.tar.gz --strip-components=1
+    rm latest.tar.gz
 fi
 
+if [ ! -f "/usr/local/bin/wp" ]; then
+    wget -q https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/local/bin/wp
+fi
+
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+
+    wp config create\
+    --path="/var/www/html"\
+    --dbname="${DB_NAME}"\
+    --dbuser="${DB_USER}"\
+    --dbhost="${DB_HOST}"\
+    --dbpass="${DB_PASSWORD}"\
+    --allow-root
+
+    wp core install \
+    --url="https://${DOMAIN_NAME}"\
+    --title="${WP_TITLE}"\
+    --admin_user="${WP_ADMIN_USER}"\
+    --admin_email="${WP_ADMIN_EMAIL}"\
+    --admin_password="${WP_ADMIN_PASSWORD}"\
+    --path="/var/www/html"\
+    --alow-root
+
+    wp user create \
+    "${WP_USER}"\
+    "${WP_USER_EMAIL}"\
+    --role=auther\
+    --path="/var/www/html"\
+    --allow-root
+fi
+
+exec "$@"
