742ff13e3ab4e6889746baa6366b8eb45db6493c
سفيان الصغير <soufianeessarhir@gmail.com>
2025-10-20 16:20:23 +0100

    start bonus part

---
+++ srcs/requirements/bonus/nginx/tools/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ ! -f "${CERTS_PATH}/nginx.cert" ] || [ ! -f "${CERTS_PATH}/nginx.key" ] ; then #the check is of restart cases
+    mkdir -p ${CERTS_PATH}
+    openssl req -nodes -x509 -days 365  -newkey rsa:2048 \
+    -out ${CERTS_PATH}/nginx.cert \
+    -keyout ${CERTS_PATH}/nginx.key \
+    -subj  "/C=MA/ST=khouribga/L=khouribga/O=1337/CN=${DOMAIN_NAME}"
+    chmod 644  "${CERTS_PATH}/nginx.cert"
+    chmod 600  "${CERTS_PATH}/nginx.key"
+fi
+
+exec "nginx"
